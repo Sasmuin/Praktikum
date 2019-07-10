00b4472369525152b02c8311aa75dd7f13abaf5f
Nils Kulawiak <nils@kulawiak.de>
2019-07-10 14:48:18 +0200

    Nils Verbesserung

--- Detektoren/Messwerte/Mischquelle_pn.py
+++ Detektoren/Messwerte/Mischquelle_pn.py
@@ -18,13 +18,23 @@ m_ec=511
 Literatur_FEPCO1 = 1332.5
 Literatur_FEPCO2 = 1173.2
 Literatur_FEP_CS = 661.6
-Literatur_FEP_CS2 = 32.2
-FEP1 = 7226.14*m+E_0
-FEP2 = 6391.91*m+E_0
+Literatur_FEP_CS2 = 59.5
+
+
+FEP2 = 7843.799*m+E_0
+FEP1 = 6906.344*m+E_0
+FEP3 = 3894.732762*m+E_0
+FEP4 = 348.7404028*m+E_0
+
+
 Compton1 = FEP1*(1-1/(1+2*FEP1/(m_ec)))
 Compton2 = FEP2*(1-1/(1+2*FEP2/(m_ec)))
+Compton3 = FEP3*(1-1/(1+2*FEP3/(m_ec)))
+Compton4 = FEP4*(1-1/(1+2*FEP4/(m_ec)))
 Backscatter1 = FEP1 - Compton1
 Backscatter2 = FEP2 - Compton2
+Backscatter3 = FEP3 - Compton3
+Backscatter4 = FEP4 - Compton4
 
 filename = Probe + "all.dat"
 infile = open(filename, 'r')  # Open file for reading
@@ -76,9 +86,16 @@ for i in range(Anzahl[0]):
 plt.vlines(Literatur_FEPCO1, ymin = 5, ymax = 8000,label = "Literatur FEP2 Co-60 1332.5 keV",color = "lime")
 plt.vlines(Literatur_FEPCO2, ymin = 5, ymax = 8000,label = "Literatur FEP1 Co-60 1173.2 keV",color = "lime",linestyle = "--")
 plt.vlines(Literatur_FEP_CS, ymin = 5, ymax = 32000,label = "Literatur FEP Cs-137 661.6 keV",color = "aqua",linestyle = "-.")
-plt.vlines(46, ymin = 50, ymax = 10000,label = "Literatur FEP Cs-137 31.8/32.2 keV",color = "aqua",linestyle = "--")
+plt.vlines(46, ymin = 50, ymax = 10000,label = "Literatur FEP Am-241 59,5 keV",color = "aqua",linestyle = "--")
 
-
+plt.vlines(Compton2, ymin = 10, ymax = 500,label = "Compon-Kante")
+plt.vlines(Compton1, ymin = 10, ymax = 500, linestyle = "--",label = "Compon-Kante")
+plt.vlines(Backscatter2, ymin = 100, ymax = 1000,color = "red",label = "Backscatter-Kante")
+plt.vlines(Backscatter1, ymin = 100, ymax = 1000,color = "red", linestyle = "--",label = "Backscatter-Kante")
+plt.vlines(Backscatter3, ymin = 100, ymax = 3000,color = "red", label = "Backscatter-Kante",linestyle ="--")
+plt.vlines(Compton3, ymin = 20, ymax = 700,label = "Compon-Kante FEP",linestyle = "--")
+plt.vlines(Backscatter4, ymin = 100, ymax = 3000,color = "red", label = "Backscatter-Kante")
+plt.vlines(Compton4, ymin = 20, ymax = 700,label = "Compon-Kante")
 
 plt.legend(loc='lower left',ncol=2,prop={'size': 7})
 plt.xlabel("Energie [keV]")
